e59c0d98f1e53cf2df3801f90f6e4e066be47fea
ElGatoPanzon <gato@elgatopanzon.io>
2026-02-18 11:41:53 -0600

    fix: component-registry: return null gracefully from get_component on missing component

--- addons/deus/classes/pipelines/component/GetComponentPipeline.gd
+++ addons/deus/classes/pipelines/component/GetComponentPipeline.gd
@@ -12,4 +12,8 @@ extends DefaultPipeline
 static func _stage_get(ctx):
 	var entity_id = ctx.world.component_registry._ensure_entity_id(ctx._node)
 	var components = ctx.world.component_registry._get_sparse_set(ctx.payload.component_name)
-	ctx.result.value = components.get_value(entity_id).duplicate(true)
+	var component = components.get_value(entity_id)
+	if component == null:
+		ctx.result.noop("component not found")
+		return
+	ctx.result.value = component.duplicate(true)
